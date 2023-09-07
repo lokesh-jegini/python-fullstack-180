a6490f3803877e12144b177da036f7934c437fcd
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-09-07 17:59:09 +0530

    variable-arithmetic-operator

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-fullstack-180
 Here we are starting from the backend course then mongodb then we move to frontend technologies
+Day-1 Bascis and setup of python
+Day-2 Variables and arithmetic operators
+      1)Program-1 by using arithmetic operator peform add,sub,mul,div,expo
+      2)area of triangle = 1/2 * base * height
+      3)#calculate age in days
---
+++ day-2-aritmeticoperator 07-09-2023/Aritmeticoperator.py
@@ -0,0 +1,30 @@
+#Arithmetic operator
+# + - * / % ** //
+# + Addition (sum of two numbers)
+number1 = 10
+number2 = 20
+print(number1 + number2)
+# - Subtraction (difference of two numbers)
+print(number1 - number2)
+# * Multiplication (product of two numbers)
+print(number1 * number2)
+# / Division (quotient of two numbers) here output is float
+print(number1 / number2)
+# % Modulus (remainder of two numbers)
+print(number1 % number2)
+# ** Exponent (power of two numbers)
+print(number1 ** number2)
+# // Floor division (quotient of two numbers) here output is integer
+print(number1 // number2)
+# area of triangle = 1/2 * base * height
+#mathematical expression required to input from user for giving output
+#based on the how many input required from user we decide the number of variable we need to declare
+base=20
+height:30
+results=1/2*(20*30)
+print(results)
+#calculate age in days
+age=25
+oneyear=365 #where we are assign data to the variable in that place only we are assign
+days=(age*oneyear)
+print(days)
