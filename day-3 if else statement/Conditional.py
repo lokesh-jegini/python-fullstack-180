c840e821a2ef5e020a6be17608e34cfcd460e809
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-09-08 18:30:23 +0530

    if and else

---
+++ day-3 if else statement/Conditional.py
@@ -0,0 +1,13 @@
+#comprasion operator with conditional 
+number1=20
+number2=30
+if number1>number2:
+    print("number1 value is greated than number2")
+
+#if else
+number1=50
+number2=20
+if number1>number2:
+    print("number1 value is greater than number2 value")
+else:
+    print("number2 value is greater thatn number1 value")
