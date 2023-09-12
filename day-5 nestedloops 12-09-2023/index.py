47f181b88ff9379afe185f27a3e69f2b972039d3
jegini lokesh <pythoncreator004@gmail.com>
2023-09-13 01:39:15 +0530

    nestedloop for patern

---
+++ day-5 nestedloops 12-09-2023/index.py
@@ -0,0 +1,9 @@
+#first we find row and columns how many are present
+#for storing row vlaue one varibale we take
+#for storing column value one variable we take
+
+for i in range(5): #range(start,end,step)
+    for j in range(5):
+     if(i>=j):
+      print("*",end=" ")
+    print("")
